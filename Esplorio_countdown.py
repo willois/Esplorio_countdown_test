9e29d1e4fc6759a0f121d387315dd189635062b4
willois <lan.buidinhkhanh@gmail.com>
2015-07-15 23:22:50 +0100

    2.0

--- Esplorio_countdown.py
+++ Esplorio_countdown.py
@@ -1,18 +1,26 @@
 from datetime import datetime
 def main():
-    a = input('Please input a date in YYYY-MM-DD format')
-    b = input('Please input the event on such deadline')
+    d = input('Please input a date in YYYY-MM-DD format')
+    e = input('Please input the event on such deadline')
 
-    def deadline_countdown(d="2015-12-25", e="Christmas"):
+    def deadline_countdown():
+        if len(d) > 0:
+            year, month, day = map(int, d.split('-'))
+            deadline = datetime(year, month, day)
+            days_till_deadline = deadline - datetime.today()
+            if len(e) > 0:
+                print('{} days until {}'.format(days_till_deadline.days, e))
+            else:
+                print('{} days until {}'.format(days_till_deadline.days, d))
+        else:
+            a = "2015-12-25"
+            b = "Christmas"
             year, month, day = map(int, a.split('-'))
             deadline = datetime(year, month, day)
             days_till_deadline = deadline - datetime.today()
-            if b == ():
-                print('{} days until {}'.format(days_till_deadline.days, d))
-            else:
-                print('{} days until {}'.format(days_till_deadline.days, e))
+            print('{} days until {}'.format(days_till_deadline.days, b))
 
-    deadline_countdown(a, b)
+    deadline_countdown()
 main()
 
 
