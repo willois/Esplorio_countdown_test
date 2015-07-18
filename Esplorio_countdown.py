3717fddcc4fd8348ce19943674fdd214518ee504
willois <lan.buidinhkhanh@gmail.com>
2015-07-18 19:32:14 +0100

    substituted 'input' with optparse

--- Esplorio_countdown.py
+++ Esplorio_countdown.py
@@ -1,33 +1,25 @@
 from datetime import datetime
+import optparse
+
 def main():
-    d = input('Please input a date in YYYY-MM-DD format')
-    e = input('Please input the event on such deadline')
+
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--deadline', action="store", dest='deadline', default='2015-12-25', type="str", help="A date in YYYY-MM-DD format")
+    parser.add_option('-e', '--event', action="store", dest='event', default='Christmas', type="str", help="The event on such deadline")
+
+    (options, args) = parser.parse_args()
 
     def deadline_countdown():
-        if len(d) > 0:
-            year, month, day = map(int, d.split('-'))
+            year, month, day = map(int, options.deadline.split('-'))
             deadline = datetime(year, month, day)
             days_till_deadline = deadline - datetime.today()
-            if len(e) > 0:
-                print('{} days until {}'.format(days_till_deadline.days, e))
+            if len(options.event) > 0:
+                print('{} days until {}'.format(days_till_deadline.days, options.event))
             else:
-                print('{} days until {}'.format(days_till_deadline.days, d))
-        else:
-            a = "12-25"
-            b = "Christmas"
-            month, day = map(int, a.split('-'))
-            if datetime.now().month > month:
-                year = datetime.now().year + 1
-                deadline = datetime(year, month, day)
-                days_till_deadline = deadline - datetime.today()
-                print('{} days until {}'.format(days_till_deadline.days, b))
-            else:
-                year = datetime.now().year
-                deadline = datetime(year, month, day)
-                days_till_deadline = deadline - datetime.today()
-                print('{} days until {}'.format(days_till_deadline.days, b))
+                print('{} days until {}'.format(days_till_deadline.days, options.deadline))
 
     deadline_countdown()
+
 main()
 
 
